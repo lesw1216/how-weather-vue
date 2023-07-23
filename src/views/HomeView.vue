20c3f1e4eae4c511a44570984ba815bb77e92a96
lesw <lesw1216@gmail.com>
2023-07-24 03:30:27 +0900

    axios 재설정, HomeView axios.get

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import axiosInstance from '@/api/axios'
+
+const userLocal = reactive({
+  city: '',
+  district: '',
+  dong: ''
+})
+
+const GetWeather = () => {
+  axiosInstance
+    .get('/weather', {
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      params: {
+        city: userLocal.city,
+        district: userLocal.district,
+        dong: userLocal.dong
+      }
+    })
+    .then((result) => {
+      console.log(result.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+</script>
 
 <template>
   <main>
     <h1>HOME</h1>
+    <v-btn @click="GetWeather">요청</v-btn>
   </main>
 </template>
