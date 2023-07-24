2824a4b62adda5550f1fd399e69abb541c52c408
lesw <lesw1216@gmail.com>
2023-07-25 01:41:47 +0900

    날씨 카드 온도, 습도 디자인 구현

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { reactive } from 'vue'
 import axiosInstance from '@/api/axios'
+import { VCardItem, VCardSubtitle } from 'vuetify/lib/components/index.mjs'
+import IconSunny from '@/components/icons/IconSunny.vue'
 
 const userLocal = reactive({
   city: '',
@@ -38,9 +40,25 @@ const GetWeather = () => {
 
 <template>
   <main>
-    <h1>HOME</h1>
     <v-btn @click="GetWeather">요청</v-btn>
-    {{ weather.TMP }}
-    {{ weather.REH }}
+    <v-card class="mx-auto" max-width="500">
+      <v-card-item>
+        <v-card-title>오늘의 날씨</v-card-title>
+        <v-card-subtitle>인천광역시 부평구 삼산동</v-card-subtitle>
+      </v-card-item>
+      <v-card-text class="py-0 d-flex justify-center">
+        <div class="text-h2">{{ weather.TMP }}&deg;</div>
+        <div class="flex flex-column">
+          <IconSunny />
+          <p class="text-center">맑음</p>
+        </div>
+      </v-card-text>
+
+      <div class="d-flex justify-center">
+        <v-list-item>
+          <v-list-item-subtitle>습도: {{ weather.REH }}%</v-list-item-subtitle>
+        </v-list-item>
+      </div>
+    </v-card>
   </main>
 </template>
