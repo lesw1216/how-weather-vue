6386589a9d762134af0aa808250cf21a71604b09
lesw <lesw1216@gmail.com>
2023-07-25 02:35:22 +0900

    주소 입력 디자인, 구현

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import { reactive } from 'vue'
 import axiosInstance from '@/api/axios'
 import { VCardItem, VCardSubtitle } from 'vuetify/lib/components/index.mjs'
 import IconSunny from '@/components/icons/IconSunny.vue'
+import IconSearch from '@/components/icons/IconSearch.vue'
 
 const userLocal = reactive({
   city: '',
@@ -16,6 +17,9 @@ const weather = reactive({
 })
 
 const GetWeather = () => {
+  console.log(userLocal.city)
+  console.log(userLocal.district)
+  console.log(userLocal.dong)
   axiosInstance
     .get('/weather', {
       headers: {
@@ -39,12 +43,36 @@ const GetWeather = () => {
 </script>
 
 <template>
-  <main>
-    <v-btn @click="GetWeather">요청</v-btn>
+  <main class="my-10">
     <v-card class="mx-auto" max-width="500">
       <v-card-item>
+        <div class="d-flex">
+          <v-text-field
+            variant="outlined"
+            class="mr-2"
+            placeholder="시/도"
+            v-bind:value="userLocal.city"
+            @input="(event) => (userLocal.city = event.target.value)"
+          ></v-text-field>
+          <v-text-field
+            variant="outlined"
+            class="mr-2"
+            placeholder="구/시/군"
+            v-bind:value="userLocal.district"
+            @input="(event) => (userLocal.district = event.target.value)"
+          ></v-text-field>
+          <v-text-field
+            variant="outlined"
+            class="mr-2"
+            placeholder="동/읍/면"
+            v-bind:value="userLocal.dong"
+            @input="(event) => (userLocal.dong = event.target.value)"
+          ></v-text-field>
+          <v-btn @click="GetWeather" class="border" size="x-large">요청</v-btn>
+        </div>
+
         <v-card-title>오늘의 날씨</v-card-title>
-        <v-card-subtitle>인천광역시 부평구 삼산동</v-card-subtitle>
+        <v-card-subtitle class="pr-2">인천광역시 부평구 삼산동 </v-card-subtitle>
       </v-card-item>
       <v-card-text class="py-0 d-flex justify-center">
         <div class="text-h2">{{ weather.TMP }}&deg;</div>
